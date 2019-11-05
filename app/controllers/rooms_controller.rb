a4f3b004d9799477b3218674ae1e782bfde6e3c6
Kazuho Kambara <>
2019-11-05 15:03:12 +0900

    Add new and create action in rooms

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,25 @@
 class RoomsController < ApplicationController
+
   def show
     @messages = Message.all
   end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(msg_params)
+    if @message.save
+      ActionCable.server.broadcast "room_channel", content: @message.content
+    else
+
+    end
+  end
+
+  private
+    def msg_params
+      params.require(:message).permit(:content)
+    end
+
 end
