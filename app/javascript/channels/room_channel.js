ef786da0a4cd90cd7ee069c8453a8dc46cb239ce
Kazuho Kambara <>
2019-11-05 15:27:37 +0900

    Add functions in room_channel.js

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -5,6 +5,7 @@ consumer.subscriptions.create("RoomChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
     console.log("Connected to the room!");
+    $('#msg').append('<div class="message"> ' + data.content + '</div>')
   },
 
   disconnected() {
@@ -15,6 +16,23 @@ consumer.subscriptions.create("RoomChannel", {
     // Called when there's incoming data on the websocket for this channel
     console.log("Recieving:")
     console.log(data.content)
+
+
+
+    let submit_messages;
+    $(document).on('turbolinks:load', function () {
+      submit_messages()
+    })
+    submit_messages = function () {
+      $('#message_content').on('keydown', function (event) {
+        if (event.keyCode == 13) {
+          $('input').click()
+          event.target.value = ''
+          event.preventDefault()
+        }
+      })
+    }
+
   },
 
   speak: function() {
